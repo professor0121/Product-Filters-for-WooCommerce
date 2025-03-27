582c006e1205c8c1220ba933e68728450a2a2b8e
Abhishek Kushwaha <141386274+professor0121@users.noreply.github.com>
2025-03-27 11:42:59 +0530

    init

---
+++ product-filters-woocommerce.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Product Filters for WooCommerce
+ * Plugin URI: https://www.linkedin.com/in/abhishek-kushwaha-5a3a49302/
+ * Author: Abhishek Kushwaha
+ * Author URI: https://www.linkedin.com/in/abhishek-kushwaha-5a3a49302/
+ * Description: Product Filters for WooCommerce is a powerful WooCommerce plugin that helps you create advanced filters for your products.
+ * Version 1.0.0
+ */
+ if(!defined('ABSPATH')){
+     exit;
+ }
+
+ echo "plugin is activated";
